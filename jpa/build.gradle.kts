2cd0c9cb6ddba83f551d29510e378daaf77c34f4
Lee Yeon Ju (Joy) <yeonzzu.lee@sk.com>
2023-03-29 20:37:22 +0900

    feat : settings for assignments of week 4

--- jpa/build.gradle.kts
+++ jpa/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.api.tasks.testing.logging.TestLogEvent.*
+
+val KOTEST_VERSION = "5.5.0"
 
 plugins {
     id("org.springframework.boot")
@@ -13,8 +16,31 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    runtimeOnly("com.h2database:h2")
+
+    compileOnly("com.h2database:h2")
+
     runtimeOnly("mysql:mysql-connector-java")
     runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test"){
+        exclude(module = "mockito-core")
+    }
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.kotest:kotest-runner-junit5:${KOTEST_VERSION}")
+    testImplementation("io.kotest:kotest-assertions-core:${KOTEST_VERSION}")
+    testImplementation("io.mockk:mockk:1.13.0")
+    testImplementation("com.ninja-squad:springmockk:4.0.0")
+    testRuntimeOnly("com.h2database:h2")
+}
+
+tasks.getByName<Test>("test") {
+    systemProperty("spring.profiles.active", "test")
+    useJUnitPlatform()
+    testLogging {
+        events(PASSED, FAILED, STANDARD_ERROR, SKIPPED)
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+        showExceptions = true
+        showCauses = true
+        showStackTraces = true
+    }
 }
--- jpa/src/test/kotlin/com/comento/jpa/JpaApplicationTests.kt
+++ jpa/src/test/kotlin/com/comento/jpa/JpaApplicationTests.kt
@@ -1,7 +1,24 @@
 package com.comento.jpa
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.provider.Arguments
 import org.springframework.boot.test.context.SpringBootTest
+import java.util.stream.Stream
+import kotlin.reflect.full.declaredMemberFunctions
+import kotlin.reflect.jvm.isAccessible
+
+// Paratmerized Test Source에 사용
+internal fun List<Any?>.toArgumentsStream() = Stream.of(*this.map{ Arguments.of (it) }.toTypedArray())
+
+// Private function call
+inline fun <reified T> T.callPrivateFunc(name: String, vararg args: Any?): Any? =
+    T::class
+        .declaredMemberFunctions
+        .firstOrNull { it.name == name }
+        ?.apply { isAccessible = true }
+        ?.call(this, *args)
+
+internal fun String.toSingleStringWithoutSpace() = this.trimIndent().replace("\n", "").replace(" ", "")
 
 @SpringBootTest
 class JpaApplicationTests {
